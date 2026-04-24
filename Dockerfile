22be9d1dfb539bbb355800163865b9e6b513a522
Alona Gavrilov <alona.gavri@gmail.com>
2026-04-23 21:47:20 -0400

    app code and Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,41 @@
+# Use AWS Lambda Python base image
+FROM public.ecr.aws/lambda/python:3.9
+
+ENV HF_HOME=/tmp/huggingface
+ENV HUGGINGFACE_HUB_CACHE=/tmp/huggingface
+ENV SENTENCE_TRANSFORMERS_HOME=/tmp/huggingface
+
+# Install build dependencies for psycopg2 if not using -binary
+
+# ---- System dependencies (IMPORTANT: upgrade libpq for SCRAM) ----
+RUN yum update -y && \
+    yum install -y \
+        gcc \
+        postgresql15 \
+        postgresql15-devel \
+    && yum clean all   
+
+# Copy requirements
+
+RUN pip install --no-cache-dir \
+    torch==2.2.2+cpu \
+    --index-url https://download.pytorch.org/whl/cpu \
+    --extra-index-url https://pypi.org/simple \
+    sentence-transformers==2.7.0 \
+    transformers==4.38.2 \
+    huggingface-hub==0.19.4 \
+    numpy==1.26.4 \
+    psycopg2-binary==2.9.9 \
+    typing_extensions \
+    requests \
+    idna
+
+# Pre-download model into image
+RUN python -c "\
+from sentence_transformers import SentenceTransformer; \
+SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2').save('/opt/model')"    
+
+# Copy function code
+COPY app.py ${LAMBDA_TASK_ROOT}
+
+CMD [ "app.handler" ]
---
+++ app.py
@@ -0,0 +1,86 @@
+import json
+import psycopg2
+import os
+
+model = None
+def get_model():
+    global model
+    if model is None:
+        from sentence_transformers import SentenceTransformer
+        model = SentenceTransformer("/opt/model")
+    return model
+
+
+def get_embedding(text):
+    if not text:
+        return None
+    else:
+        model = get_model()
+        return model.encode(text).tolist()
+
+def get_command(i1):
+
+    query = f"""
+    SELECT 
+        title,
+        release_date,
+        genres,
+        overview,
+        keywords,
+        1-(embedding_combined <-> %s::vector) as similarity 
+    FROM movies_kaggle_db5000
+    ORDER BY similarity DESC
+    LIMIT 5;
+    """
+    return query, (i1,)
+    
+
+def handler(event, context):
+    
+    # Parse JSON body safely
+    body = json.loads(event.get("body", "{}"))
+    
+    # embed the query:
+    genre = body.get("genre", None)
+    overview = body.get("overview", None)
+    keywords = body.get("keywords", None)
+
+    combined_input = " ".join(part for part in [genre, overview, keywords] if part)
+    if not combined_input:
+         return {'statusCode': 200, 'headers': {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Headers": "Content-Type",
+                    "Access-Control-Allow-Methods": "OPTIONS,POST"
+                },'body': json.dumps({})}
+
+    combined_input_embedded = get_embedding(combined_input)     
+    execute_command, params = get_command(combined_input_embedded)
+
+    conn = psycopg2.connect(
+        host=os.environ['DB_HOST'],
+        database=os.environ['DB_NAME'],
+        user=os.environ['DB_USER'],
+        password=os.environ['DB_PASSWORD'],
+        port=5432
+    )
+    cur = conn.cursor()
+    cur.execute(execute_command, params)
+    rows = cur.fetchall()
+    formatted_rows = []
+    for row in rows:
+        formatted_rows.append({"title":row[0],
+                                "release_date":row[1],
+                                "genre":row[2],
+                                "overview":row[3],
+                                "keywords":row[4],
+                                "sim_score":round(row[5],3)})             
+    cur.close()
+    conn.close()
+    return {'statusCode': 200, 'headers': {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "OPTIONS,POST"
+    },'body': json.dumps(formatted_rows)}
+
+
+
